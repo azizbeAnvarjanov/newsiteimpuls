de1a9baf7d08702dc94279fa014e357ae406c264
azizbeAnvarjanov <azizbeanvarjanov@gmail.com>
2025-01-03 10:00:37 +0500

    change mobile navbar and image mb

--- src/app/(customComponents)/MobileNavbar.jsx
+++ src/app/(customComponents)/MobileNavbar.jsx
@@ -10,74 +10,105 @@ import {
 import { Menu } from "lucide-react";
 import { Link } from "@/i18n/routing";
 import { useTranslations } from "next-intl";
-import { Separator } from "@/components/ui/separator";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const MobileNavbar = () => {
   const t = useTranslations("Navbar");
   const links = [
     {
-      name: t("rektor-murojati"),
-      href: "/pages/rector-freeze",
-    },
-    {
-      name: t("institut-tarixi"),
-      href: "/pages/institut-of-history",
-    },
-    {
-      name: t("ilmiy-kengash"),
-      href: "/pages/scientific-council",
-    },
-    {
-      name: t("institut-mamuriyati"),
-      href: "/pages/administration-institute",
-    },
-    {
-      name: t("institutga-sayohat"),
-      href: "/pages/trip-the-institute",
-    },
-    {
-      name: t("institut-xaritasi"),
-      href: "/pages/institute-map",
-    },
-    {
-      name: t("faq"),
-      href: "/pages/faq",
+      name: t("institut"),
+      links: [
+        {
+          name: t("rektor-murojati"),
+          href: "/pages/rector-freeze",
+        },
+        {
+          name: t("institut-tarixi"),
+          href: "/pages/institut-of-history",
+        },
+        {
+          name: t("ilmiy-kengash"),
+          href: "/pages/scientific-council",
+        },
+        {
+          name: t("institut-mamuriyati"),
+          href: "/pages/administration-institute",
+        },
+        {
+          name: t("institutga-sayohat"),
+          href: "/pages/trip-the-institute",
+        },
+        {
+          name: t("institut-xaritasi"),
+          href: "/pages/institute-map",
+        },
+        {
+          name: t("faq"),
+          href: "/pages/faq",
+        },
+      ],
     },
     {
       name: t("biz-haqimizda"),
-      href: "/pages/about-us",
+      links: [
+        {
+          name: t("biz-haqimizda"),
+          href: "/pages/rector-freeze",
+        },
+        {
+          name: t("hamkor-universitetlar"),
+          href: "/pages/partner-universities",
+        },
+        {
+          name: t("asosiy-qadriyatlar"),
+          href: "/pages/core-values",
+        },
+        {
+          name: t("asosiy-meyoriy-hujjatlar"),
+          href: "/pages/basic-documents",
+        },
+      ],
     },
     {
-      name: t("hamkor-universitetlar"),
-      href: "/pages/partner-universities",
+      name: t("xalqaro-faoliyat"),
+      links: [
+        {
+          name: t("xalqaro-hamkorlik"),
+          href: "/pages/international-cooperation",
+        },
+        {
+          name: t("xalqaro-talabalar"),
+          href: "/pages/international-students",
+        },
+        {
+          name: t("almashinuv-dasturi"),
+          href: "/pages/exchange",
+        },
+      ],
     },
     {
-      name: t("asosiy-qadriyatlar"),
-      href: "/pages/core-values",
-    },
-    {
-      name: t("asosiy-meyoriy-hujjatlar"),
-      href: "/pages/basic-documents",
-    },
-    {
-      name: t("xalqaro-hamkorlik"),
-      href: "/pages/international-cooperation",
-    },
-    {
-      name: t("xalqaro-talabalar"),
-      href: "/pages/international-students",
-    },
-    {
-      name: t("almashinuv-dasturi"),
-      href: "/pages/exchange",
-    },
-    {
-      name: t("bakalavr-grandi"),
-      href: "/pages/bachelors-degree",
+      name: t("grandlar"),
+      links: [
+        {
+          name: t("bakalavr-grandi"),
+          href: "/pages/bachelors-degree",
+        },
+      ],
     },
     {
       name: t("student-union"),
-      href: "/pages/student-union",
+      links: [
+        {
+          name: t("student-union"),
+          href: "/pages/student-union",
+        },
+      ],
     },
   ];
 
@@ -90,15 +121,27 @@ const MobileNavbar = () => {
         <SheetContent className="overflow-y-scroll">
           <SheetHeader>
             <SheetDescription className="text-left text-black flex flex-col gap-2">
-              {links.map((item, idx) => (
-                <Link
-                  key={idx}
-                  className="border py-2 px-4 rounded-md hover:bg-slate-300"
-                  href={item.href}
-                >
-                  {item.name}
-                </Link>
-              ))}
+              <Accordion type="single" collapsible>
+                {links.map((item, idx) => (
+                  <AccordionItem
+                    key={idx}
+                    className="border border-b-[1px] border-t-0 border-x-0"
+                    value={`item${idx + 1}`}
+                  >
+                    <AccordionTrigger>{item.name}</AccordionTrigger>
+                    {item.links.map((link, idx) => (
+                      <AccordionContent className="" key={idx}>
+                        <Link
+                          className="w-full h-full flex p-2 rounded-md bg-white hover:underline"
+                          href={link.href}
+                        >
+                          {link.name}
+                        </Link>
+                      </AccordionContent>
+                    ))}
+                  </AccordionItem>
+                ))}
+              </Accordion>
             </SheetDescription>
           </SheetHeader>
         </SheetContent>
